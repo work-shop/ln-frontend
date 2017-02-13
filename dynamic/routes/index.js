b9b9b5a78c1854ff0b408c98b008eb4189478b3b
Nic Schumann <nicolas_schumann@brown.edu>
2017-02-13 15:32:30 -0500

    [Index Route] Built out the index route; proved the base-route strategy.

--- dynamic/routes/index.js
+++ dynamic/routes/index.js
@@ -1,32 +1,27 @@
 "use strict";
 
-var route = require('./base-route.js');
+var util = require('util');
+var base = require('./base-route.js')();
+var async = require('async');
+
+var restructureIndex = require('../structures/restructure-index.js');
 
 /**
  *
  *
  */
 module.exports = function( wp, config, globals ) {
-    return route(
-        [ wp.namespace( 'acf/v2' ).options(), wp.thoughts() ],
-        [
-            function( options, thoughts, callback ) {
-                globals.log.log( options, 'options-cb1');
-                globals.log.log( thoughts, 'thoughts-cb1' );
+    return base.route(
+        /**
+         * Get initial set of resources we need to render the page.
+         */
+        [ wp.namespace( 'acf/v2' ).options().embed() ],
 
-                callback( null, options );
-
-            },
-
-            function( options, thoughts, callback ) {
-                globals.log.log( options, 'options-cb2');
-                globals.log.log( thoughts, 'thoughts-cb2' );
-
-                callback( null, thoughts );
-            }
-
-
-        ],
+        /**
+         * Process the options to resolve the thoughts that should
+         * be embedded on the main page.
+         */
+        [ getThoughtGroupsForOptions ],
         /**
          * Success Case. All of the needed resources were properly resolved,
          * And the data is available for use immediately in the callback, along
@@ -35,13 +30,13 @@ module.exports = function( wp, config, globals ) {
          * @param req the Express Request Object
          * @param res the Express Response Object
          * @param options JSON, the requested options data
-         * @param thoughts JSON, the requested thoughts object
          */
-        function( req, res, options, thoughts ) {
-            globals.log.log( options.acf.site_statement, 'route-success-handler' );
-            globals.log.log( thoughts[0].slug, 'route-success-handler' );
+        function( req, res, options ) {
 
-            res.render('index.html', {});
+            globals.log.log( 'Successful request to index.', 'route-index:success-handler');
+
+            res.render('index.html', restructureIndex( options ) );
+
         },
         /**
          * Error Case. Something went wrong reconciling one or all of the
@@ -54,8 +49,44 @@ module.exports = function( wp, config, globals ) {
          */
         function( req, res, err ) {
 
-            globals.log.error( err, 'route-index-error-handler');
+            globals.log.error( err, 'route-index:error-handler');
+
             res.render('404.html', {error_code: 500, description: err.message });
 
         });
+
+        /**
+         *
+         *
+         *
+         */
+        function getThoughtGroupsForOptions( options, callback ) {
+
+            async.map((options.acf.thought_groups || []), function( group, next ){
+
+                async.map(group.thoughts, function( thought, next ) {
+
+                    wp.thoughts().id( thought.ID ).embed()
+                      .then( function( data ) { next( null, data ); })
+                      .catch( function( err ) { next( err ); });
+
+                }, function(err, result) {
+
+                    group.thoughts = result;
+
+                    next( err, group );
+
+                });
+
+            }, function( err, result ) {
+
+            // globals.log.log( util.inspect( result[0].thoughts ), 'get-thought-groups' );
+
+                options.acf.thought_groups = result;
+
+                callback( err, options );
+
+            });
+        }
+
 };
