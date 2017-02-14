f1b3ca74002ff175c5e29f6eb3aa037d7e911ef7
Nic Schumann <nicolas_schumann@brown.edu>
2017-02-14 18:43:29 -0500

    [Routes] registered handlers in application.

--- dynamic/routes.js
+++ dynamic/routes.js
@@ -9,9 +9,16 @@
  */
 
 var index = require('./routes/index.js');
+var about = require('./routes/about.js');
+var thoughts = require('./routes/thoughts.js');
+var thought = require('./routes/thought.js');
 
 module.exports = function( express, app, config, globals ) {
 
     app.get('/', index( globals.wp, config, globals ) );
+
+    app.get('/thoughts/:id', thought( globals.wp, config, globals ));
+
+    app.get('/about', about( globals.wp, config, globals ));
 
 };
