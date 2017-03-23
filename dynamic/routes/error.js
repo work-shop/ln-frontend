4f10c89be9aa4560ed66ee6320d4dda7a2d08824
Nic Schumann <nicolas_schumann@brown.edu>
2017-03-23 16:04:46 -0400

    [Error Template] build out the error template and route.

--- dynamic/routes/error.js
+++ dynamic/routes/error.js
@@ -1,6 +1,8 @@
 "use strict";
 
+var base = require('./generic/base-route.js')();
 var messages = require('../structures/error-message-structure.js');
+var baseStructure = require('../structures/base-structure.js');
 
 /**
  *
@@ -9,20 +11,58 @@ var messages = require('../structures/error-message-structure.js');
  */
 module.exports = function( errorNumber ) {
     return function( wp, config, globals ) {
-        return function( req,res ) {
 
-            var response = messages["" + errorNumber + ""];
+        var urlReplace = require('../utilities/resource-map.js')( config );
 
-            if ( typeof response === "function" ) {
+        return base.route(
+            /**
+             * Get initial set of resources we need to render the page.
+             */
+            [ wp.namespace( 'acf/v2' ).options().embed() ],
 
-                res.render('error.html',  {error_code: errorNumber, description: response( req ) });
+            /**
+             * Success Case. All of the needed resources were properly resolved,
+             * And the data is available for use immediately in the callback, along
+             * with the request and the response.
+             *
+             * @param req the Express Request Object
+             * @param res the Express Response Object
+             * @param options JSON, the requested options data
+             */
+            function( req, res, options ) {
 
-            } else {
+                globals.log.log( '404 response.', 'route-404:success-handler');
 
-                res.render('error.html', {error_code: errorNumber, description: response} );
+                var response = messages["" + errorNumber + ""];
 
-            }
+                res.status( errorNumber );
 
-        };
+                if ( typeof response === "function" ) {
+
+                    res.render('error.html',  urlReplace( baseStructure( {error_code: errorNumber, description: response }, options, globals ) ));
+
+                } else {
+
+                    res.render('error.html', urlReplace( baseStructure( {error_code: errorNumber, description: response }, options, globals ) ) );
+
+                }
+
+            },
+            /**
+             * Error Case. Something went wrong reconciling one or all of the
+             * requested resources. The error is supplied to the callback
+             * and *you* handle the problem.
+             *
+             * @param req the Express Request Object
+             * @param res the Express Response Object
+             * @param err Error the reason for failure.
+             */
+            function( req, res, err ) {
+
+                globals.log.error( err, 'route-index:error-handler');
+
+                res.render('error.html', {error_code: 500, description: err.message });
+
+            });
     };
-}
+};
